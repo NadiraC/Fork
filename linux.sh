0a879162a739b2c5618a04c985fc5f7b5cd16c27
Nadira Clifton <clifton7nadira7@gmail.com>
2022-11-28 17:29:04 +0000

    Added new line to the script

--- linux.sh
+++ linux.sh
@@ -17,6 +17,9 @@ sleep 2
 echo "My IP address is:"
 echo "-----------------------------"
 sleep 2
+echo "Your email is:" && email
+echo "-----------------------------"
+sleep 2
 curl ifconfig.me
 
 This is week 4 - project test
